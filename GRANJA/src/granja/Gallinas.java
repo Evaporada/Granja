11bd6b05a64b5fdc729cae3055e24502c0470a08
Eva <Eva@DESKTOP-IIJ1J63>
2020-03-30 21:55:23 +0200

    mejorado

--- GRANJA/src/granja/Gallinas.java
+++ GRANJA/src/granja/Gallinas.java
@@ -3,21 +3,58 @@ package granja;
 import java.util.Arrays;
 
 public class Gallinas extends Animales {
-	protected int huevos;
-	protected int mes[];
+	protected int[] huevos;
 
 	public Gallinas() {
-		
+		super();
+		huevos = huevosAle();
+
 	}
 
-	public Gallinas(int[] mes) {
-		this.huevos = (int) (Math.random() * 5 + 7);
-		this.mes = mes;
+	public Gallinas(int[] huevos) {
+		super();
+		this.huevos = huevos;
 	}
+	
+	public Gallinas(String nombre, String iD, Fecha fecha, int[] huevos2) {
+		super(nombre, iD, fecha);
+		this.huevos=huevos2;
+	}
+
+	public int[] huevosAle() {
+		int[] huevos = new int[30];
+		for (int i = 0; i < huevos.length; i++) {
+			huevos[i] = (int) (Math.random() * 5 + 7);
+		}
+		return huevos;
+	}
+
+	public int[] getHuevos() {
+		return huevos;
+	}
+
+	public void setHuevos(int[] huevos) {
+		this.huevos = huevos;
+	}
+
+	
 
 	@Override
 	public String toString() {
-		return "Gallinas [huevos=" + huevos + ", mes=" + Arrays.toString(mes) + "]";
+		return "Gallinas [huevos diarios=" + Arrays.toString(huevos) + ", nombre=" + nombre + ", ID=" + ID + ", fecha="
+				+ fecha + "]";
+	}
+
+	@Override
+	public boolean esRentable() {
+		boolean rentable = false;
+		for (int i = 0; i < huevos.length - 1; i++) {
+			int total = huevos[i] + huevos[i + 1];
+			if ((total / 30) >= 3) {
+				rentable = true;
+			}
+		}
+		return rentable;
 	}
 
 }
--- GRANJA/src/granja/Vacas.java
+++ GRANJA/src/granja/Vacas.java
@@ -5,8 +5,19 @@ public class Vacas extends Animales {
 	protected float kg;
 
 	public Vacas() {
-		this.litros = (int) ((Math.random() * 20) + 30);
-		this.kg = (int) ((Math.random() * 80) + 50);
+		this.litros = (int) ((Math.random() * 600) + 100);
+		this.kg = (int) ((Math.random() * 100) + 50);
+
+	}
+
+	public Vacas(String nombre, String iD, Fecha fecha, float peso, int litros2) {
+		super(nombre, iD, fecha);
+		this.litros=litros2;
+		this.kg=peso;
+	}
+
+	public int getLitros() {
+		return litros;
 	}
 
 	public void setLitros(int litros) {
@@ -17,10 +28,23 @@ public class Vacas extends Animales {
 		return kg;
 	}
 
+	public void setKg(float kg) {
+		this.kg = kg;
+	}
 
 	@Override
 	public String toString() {
-		return "Vacas [litros=" + litros + ", kg=" + kg + "]";
+		return "Vacas [litros=" + litros + ", kg=" + kg + ", nombre=" + nombre + ", ID=" + ID + ", fecha=" + fecha
+				+ "]";
+	}
+
+	@Override
+	public boolean esRentable() {
+		boolean rentable = true;
+		if (litros >= 500) {
+			rentable = true;
+		}
+		return rentable;
 	}
 
 }
